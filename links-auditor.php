84aa2779ef949867f70f3230fe4b9861781c9689
tonyspiro <tonyspiro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-02 22:34:00 +0000

    added https capability
    git-svn-id: https://plugins.svn.wordpress.org/links-auditor/trunk@925412 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- links-auditor.php
+++ links-auditor.php
@@ -41,23 +41,25 @@ error_reporting(E_ALL);
 
 include('controllers.php');
 
-/// Test page for redirect
-
 $siteurl = get_bloginfo('siteurl');
 
+function getUrl() {
+  $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
+  $url .= $_SERVER["REQUEST_URI"];
+  return $url;
+}
+
+$actual_link = getUrl();
 
 $all_redirects = $redirects->getAll();
-
-/// Get slug minus siteurl
-$la_slug = str_replace($siteurl, "", $_SERVER['REQUEST_URI']);
 
 if($all_redirects){
 	foreach($all_redirects as $redirect_id){
 		$la_redirect = $redirects->getFields($redirect_id);
 		$la_old_link = str_replace($siteurl, "", $la_redirect['old_link']);
 		$la_new_link = str_replace($siteurl, "", $la_redirect['new_link']);
-		if($la_slug == $la_old_link){
-			header("Location: " . $la_new_link);
+		if($actual_link == $siteurl . $la_old_link){
+			header("Location: " . $siteurl . $la_new_link);
 			die();
 		}
 	}
